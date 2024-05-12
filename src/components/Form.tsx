294ad748008148d363edc40f895c0a4732b716d1
YazimelNiclis <42756188+YazimelNiclis@users.noreply.github.com>
2024-05-12 18:50:12 -0300

    sass

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useForm from "../hooks/useForm";
+import "../styles/main.scss";
 
 const Form = () => {
   const { values, handleChange, resetForm } = useForm({ name: "", email: "" });
@@ -11,23 +12,25 @@ const Form = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        value={values.name}
-        onChange={handleChange}
-        placeholder="Name"
-      />
-      <input
-        type="email"
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-        placeholder="Email"
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
